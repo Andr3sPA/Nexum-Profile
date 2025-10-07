84b2f971713e6ae2c072a166e5aee5511d325966
sneyderbg <sneyder.buitrago@udea.edu.co>
2025-10-07 18:24:15 -0500

    fix: update Roles to match the other micros

--- src/main/java/co/edu/udea/nexum/profile/auth/domain/utils/enums/RoleName.java
+++ src/main/java/co/edu/udea/nexum/profile/auth/domain/utils/enums/RoleName.java
@@ -1,20 +1,29 @@
 package co.edu.udea.nexum.profile.auth.domain.utils.enums;
 
 public enum RoleName {
-    /**
-     * An administrative can add, read, modify and filter graduates, but cannot remove it
-     */
-    ADMINISTRATIVE,
-    /**
-     * A graduate can modify their information and add updates
-     */
-    GRADUATE,
-    /**
-     * A Dean can list and filter to search graduates
-     */
-    DEAN,
-    /**
-     * An employer can search and hire graduates
-     */
-    EMPLOYER,
+  /**
+   * Someone with Admin role can do anything in the system
+   */
+  ADMIN,
+  /**
+   * An administrative can add, read, modify and filter graduates, but cannot
+   * remove it
+   */
+  ADMINISTRATIVE,
+  /**
+   * A pre graduate only can see their own information
+   */
+  PRE_GRADUATE,
+  /**
+   * A graduate can modify their information and add updates
+   */
+  GRADUATE,
+  /**
+   * A Dean can list and filter to search graduates
+   */
+  DEAN,
+  /**
+   * An employer can search and hire graduates
+   */
+  EMPLOYER,
 }
